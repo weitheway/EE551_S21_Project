ff21198152b5484421ff92f6eddea2891a07be28
weitheway <hiwei103098@gmail.com>
2021-05-19 16:16:11 -0400

    HTML changes

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import datetime
-from flask import Flask, render_template, request, url_for, 
+from flask import Flask, render_template, request, url_for 
 import db 
 
 app = Flask(__name__)
 
 db.create_tables()
 
-@app.route("/home" , methods=["GET","POST"])
+@app.route("/home")
 @app.route("/", methods=["GET","POST"])
 def home():
+    return render_template("home.html", entries=db.retrieve_entries())
+
+@app.route("/admin", methods=["GET","POST"])
+def admin():
     if request.method == "POST":
         entry_content = request.form.get("content")
         db.create_entry(entry_content, datetime.datetime.today().strftime("%m/%d %H:%M:%S"))
-    return render_template("home.html", entries=db.retrieve_entries())
+    return render_template("admin.html", entries=db.retrieve_entries())
 
 @app.route("/aboutme")
 def aboutme():
